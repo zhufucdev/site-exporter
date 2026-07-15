ab57b9b3d378c0485b9e89e81307e7a5bbb42fc6
zhufucdev <zhufuzhufu1@gmail.com>
2026-07-15 17:04:06 +0800

    support custom interface binding

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,22 @@ var debug_allocator: std.heap.DebugAllocator(.{
 }) = .{};
 
 pub fn main(init: std.process.Init) !void {
+    const args = init.minimal.args.vector;
+    var listen_interface: ?[*:0]const u8 = null;
+    var listen_port: usize = 8080;
+    if (args.len >= 2) {
+        listen_interface = args[1];
+    }
+    if (args.len >= 3) {
+        listen_port = std.fmt.parseUnsigned(@TypeOf(listen_port), std.mem.span(args[2]), 10) catch {
+            std.log.err("Unknown port: {s}", .{args[2]});
+            return;
+        };
+    }
+    if (args.len >= 4) {
+        std.log.warn("Remaining {d} arguments are ignored", .{args.len - 3});
+    }
+
     const gpa, const is_debug = switch (builtin.mode) {
         .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
         .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
@@ -31,8 +47,8 @@ pub fn main(init: std.process.Init) !void {
     try App.register(&metrics_endpoint);
 
     try App.listen(.{
-        .interface = "0.0.0.0",
-        .port = 8080,
+        .interface = listen_interface,
+        .port = listen_port,
     });
 
     zap.start(.{
